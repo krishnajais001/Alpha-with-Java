34c0a167d9c97b9533014fb03cfd07ab09d720f8
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-22 11:55:42 +0530

    Output in Java

---
+++ useOfOutputIn.java
@@ -0,0 +1,7 @@
+public class useOfOutputIn {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World\n"); // it prints only a line  "\n" escape character for next line
+        System.out.println("Hello World"); // it prints and move cursor to a next line
+    }
+}
