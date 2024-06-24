98d9be932d457c16279f31c0d919e48bad35b80d
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-24 15:10:24 +0530

    Add files via upload

---
+++ prog2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class prog2 {
+public static void main(String[] args) {
+    Scanner val=new Scanner(System.in);
+    int a,b,mul;
+    System.out.println("Enter Value of A :");
+    a=val.nextInt();
+    System.out.println("Enter Value of B :");
+    b=val.nextInt();
+    mul=a*b;
+    System.out.println("Sum :" +mul);
+
+}
+    
+}
