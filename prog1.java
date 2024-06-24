470a3a94768df3bdebe038b69f19138777e4782e
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-24 15:10:07 +0530

    Sum of Two Number

---
+++ prog1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class prog1 {
+public static void main(String[] args) {
+    Scanner val=new Scanner(System.in);
+    int a,b,sum;
+    System.out.println("Enter Value of A :");
+    a=val.nextInt();
+    System.out.println("Enter Value of B :");
+    b=val.nextInt();
+    sum=a+b;
+    System.out.println("Sum :" +sum);
+
+}
+    
+}
