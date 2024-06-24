676eabf6f7b2d1047d20cad6cd9c816cf52f2bca
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-24 15:15:52 +0530

    Implicit

---
+++ prog4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class prog4 {
+public static void main(String[] args) {
+    Scanner val=new Scanner(System.in);
+   int a=5;
+   float b=a; // implicit
+   System.out.println("A :"+a); 
+   System.out.println("B :"+b);
+
+}
+    
+}
