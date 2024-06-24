71582417fb7c3fd34a55f0618baf971fc1a9bf2e
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-24 15:13:33 +0530

    Area of Circle

---
+++ prog3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class prog3 {
+public static void main(String[] args) {
+    Scanner val=new Scanner(System.in);
+    double r,area;
+ System.out.println("Enter Radius :");
+ r=val.nextDouble();
+ area=3.14*r*r;
+ System.out.println("Area of circle is :"+area);
+
+}
+    
+}
