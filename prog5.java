e90d8271581025c0b4da447b9534a3ff3e58248e
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-06-24 15:17:22 +0530

    Add files via upload

---
+++ prog5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class prog5 {
+public static void main(String[] args) {
+    Scanner val=new Scanner(System.in);
+   float a=5.25f;
+   int b=(int)a; // Explicit
+   System.out.println("A :"+a); 
+   System.out.println("B :"+b);
+
+}
+    
+}
