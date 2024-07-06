ba8275f0bbc7e53a70186697919af626131ce82c
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2024-07-06 17:26:13 +0530

    Add files via upload

---
+++ baseics.java
@@ -0,0 +1,9 @@
+public class baseics {
+    public static void main(String[] args) {
+      int n=1;
+        do {
+            System.out.println("Hello Krishna");
+            n++;
+        } while (n<=10);
+    }
+}
